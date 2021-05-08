ed5558f3776f6fecf87e0e8aa632b65d4226bdaa
Pablo Martín Escudero <escuderopablo@hotmail.com.ar>
2021-05-08 11:59:15 -0300

    'material-ui'

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css"
+import NavBar from './componentes/navBar';
+import React, {Component, Fragment} from 'react';
+
+function App() {
+  return (
+    <div className = "App">
+  <NavBar/>
+    </div>
+   
+  );
+}
+
+export default App;
