6002d9ce238b26281adb9c460c26a56224021e36
Pablo Martín Escudero <escuderopablo@hotmail.com.ar>
2021-05-15 15:28:28 -0300

    Botonera carrito-stock

--- src/Componentes/ItemCount.jsx
+++ src/Componentes/ItemCount.jsx
@@ -1,22 +1,42 @@
-import {React,Component, useState} from 'react'
-import '../Styles/ItemCount.css'
-const ItemCount = () => {
+import {React, useState} from 'react'
+import '../Styles/ItemCount.css';
+
+let stock = 5;
+const ChangeStock = ()=>{
     const [amount, setAmount] = useState(stock);
-    let stock = 10
-    const itemCount = (stock, initial, )=>{
-        console.log(stock)
+    const [counter, setCounter] =useState(0)
+
+    const itemAdd = (x)=>{
+        if(counter === stock) return console.log('You cannot add more items')
+        setAmount(amount-1);
+        setCounter(counter+x);
+    };
+
+
+    const itemDecrease = (stock)=>{
+        if(counter === 0 || amount === stock) return console.log('You cannot return more items')
+        setAmount(amount+1)
+        setCounter(counter-1)
     }
+    return {amount, itemAdd, itemDecrease, counter}
+}
+
+const ItemCount = () => {
+    const {amount, itemAdd, itemDecrease, counter} = ChangeStock();
     return (
         <div className="itemCount">
-            <button onCliclk={()=> itemCount()-1}>
-                Add
-            </button>
-            <button>
-                Decrease
-            </button>
-            <span>{amount}</span>
+            <div className = "buttons">
+                <button onClick={()=> itemAdd(1) }>
+                    ADD
+                </button>
+                <span>{counter}</span>
+                <button onClick={()=>itemDecrease(-1)}>
+                    DECREASE
+                </button>
+            </div>
+            <span className="stock">Available stock: {amount}</span>
         </div>
     )
 }
 
-export default ItemCount
+export default ItemCount;
