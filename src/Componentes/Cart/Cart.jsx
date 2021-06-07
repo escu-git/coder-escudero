e369ac85829a2d96121c63426d586111172cee85
Pablo Martín Escudero <escuderopablo@hotmail.com.ar>
2021-06-06 22:04:09 -0300

    Cart and purchase features

--- src/Componentes/Cart/Cart.jsx
+++ src/Componentes/Cart/Cart.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
+//'/cart'
 const Cart = () => {
     return (
         <div>
-            <h1>THIS IS THE CART SECTION</h1>
+            <h1>THIS IS THE CART SECTION</h1> 
         </div>
     )
 }
